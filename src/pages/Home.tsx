f7407dd1c2ce80d962d820d857cbd15d82e91959
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-02-15 18:43:18 +0600

    redux store created

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-            <h2 className="bg-red-500">home page</h2>
+            <h2 className="bg-red-600">home page</h2>
         </div>
     );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <div>
-            login page
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Email:</label>
+        <input type="email" {...register("email")} required />
+
+        <label>Password:</label>
+        <input type="password" {...register("password")} required />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
