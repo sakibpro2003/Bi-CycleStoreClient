65d9853c081c151570c83f19cd87d86e209a2ffc
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-02-16 01:21:47 +0600

    login page designed with tailwindUI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ const Home = () => {
     return (
         <div>
             <h2 className="bg-red-600">home page</h2>
+            <button className="btn">Button</button>
         </div>
     );
 };
